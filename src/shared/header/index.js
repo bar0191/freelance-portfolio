92de1b854d11cb5aa57e1663308e56aec235c559
bar0191 <brandonreid2@my.unt.edu>
2017-07-22 18:32:54 -0500

    mobile navbar updated, added theme color to site (jadegreen)

--- src/shared/header/index.js
+++ src/shared/header/index.js
@@ -5,8 +5,7 @@ import './style.scss';
 
 export default (props) => (
   <header className="site-head">
-    <div>
-      <IconMenu className="menu-icon" size="24" color="#fff" onClick={props.toggleNav} style={{cursor: 'pointer'}}/>
-    </div>
+    <div className="site-head-title">Brandon Reid</div>
+    <IconMenu className="menu-icon" size="34" color="#00FF96" onClick={props.toggleNav} style={{cursor: 'pointer'}}/>
   </header>
 );
