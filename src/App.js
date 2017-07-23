be65e705f6e87768a434cab922560ab253624707
bar0191 <brandonreid2@my.unt.edu>
2017-07-22 21:08:26 -0500

    started on home page styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './shared/nav';
 import SiteHead from './shared/header';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 
+
 //styles
 import './App.scss';
 
@@ -15,19 +16,19 @@ export default class App extends Component {
   toggleNav = (e) => {
     e.preventDefault();
     this.setState({navMini: !this.state.navMini});
-  }
+  };
   hideNav = (e) => {
     e.stopPropagation();
     e.preventDefault();
     this.setState({navMini: false})
-  }
+  };
   render() {
     let navMini = this.state.navMini;
+
     return (
       <div className="app-wrapper">
         <Nav mini={navMini} />
-        <div className="content-container ">
-          <div className="menu-dropshadow" style={{display: (navMini ? 'block' : 'none')}} onClick={this.hideNav}/>
+        <div className="content-container">
           <SiteHead toggleNav={this.toggleNav}/>
           {this.props.children}
         </div>
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import './style.scss';
+
+const headerStyle = {
+  margin: 0,
+  color: '#00FF96'
+};
+
+const paraStyle = {
+  margin: '10px',
+  color: '#333'
+};
+
 export default () => (
-  <div>
-    home page
+
+  <div className="home-container">
+    <div className="sub-page">
+      <section className="table">
+        <div>
+          <h1 style={headerStyle}>Brandon Reid</h1>
+          <p style={paraStyle}>Junior Full Stack Developer</p>
+        </div>
+      </section>
+    </div>
   </div>
 );
