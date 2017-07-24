c298253061ddf01c981056ab6b8a7931f10eda3a
bar0191 <brandonreid2@my.unt.edu>
2017-07-24 17:09:01 -0500

    removing some hidden files, and .idea

--- src/shared/nav/index.js
+++ src/shared/nav/index.js
@@ -37,12 +37,6 @@ class NavList extends React.Component {
       selected: (c.indexOf('selected') >= 0) ? '' : index
     })
   };
-  handleOpen = (index, e) => {
-    e.stopPropagation();
-    this.setState({
-      selected: index
-    })
-  };
 
   render() {
     const NavStyle = {
@@ -51,11 +45,21 @@ class NavList extends React.Component {
     return (
       <div style={NavStyle}>
         <ul className="nav-list">
-          <li onClick={this.props.hideNav}><IndexLink activeClassName="active" to="/"><IconHome className="icon"/><br/>Home</IndexLink></li>
-          <li onClick={this.props.hideNav}><Link activeClassName="active" to="/about"><IconUser className="icon"/><br/>About Me</Link></li>
-          <li onClick={this.props.hideNav}><Link activeClassName="active" to="/resume"><IconCard className="icon"/><br/>Resume</Link></li>
-          <li onClick={this.props.hideNav}><Link activeClassName="active" to="/portfolio"><IconCase className="icon"/><br/>Portfolio</Link></li>
-          <li onClick={this.props.hideNav}><Link activeClassName="active" to="/contact"><IconMail className="icon"/><br/>Contact</Link></li>
+          <li onClick={this.props.hideNav}>
+            <IndexLink activeClassName="active" to="/"><IconHome className="icon"/><br/>Home</IndexLink>
+          </li>
+          <li onClick={this.props.hideNav}>
+            <Link activeClassName="active" to="/about"><IconUser className="icon"/><br/>About Me</Link>
+          </li>
+          <li onClick={this.props.hideNav}>
+            <Link activeClassName="active" to="/resume"><IconCard className="icon"/><br/>Resume</Link>
+          </li>
+          <li onClick={this.props.hideNav}>
+            <Link activeClassName="active" to="/portfolio"><IconCase className="icon"/><br/>Portfolio</Link>
+          </li>
+          <li onClick={this.props.hideNav}>
+            <Link activeClassName="active" to="/contact"><IconMail className="icon"/><br/>Contact</Link>
+          </li>
         </ul>
       </div>
     )
@@ -65,7 +69,11 @@ class NavList extends React.Component {
 
 export default (props) => (
   <nav className={`site-nav ${props.mini ? 'mini' : ''}`} role="navigation">
-    <ScrollArea className="nav-list-container" horizontal={false} verticalScrollbarStyle={{width: '4px', marginLeft: '10px'}}>
+    <ScrollArea
+      className="nav-list-container"
+      horizontal={false}
+      verticalScrollbarStyle={{width: '4px', marginLeft: '10px'}}
+    >
     <NavHead/>
     <NavList {...props}/>
     </ScrollArea>
