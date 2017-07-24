2da1f0ec361956e72f6aa0250c07614c3b3c96f1
bar0191 <brandonreid2@my.unt.edu>
2017-07-23 22:19:49 -0500

    added description section to project pages

--- src/views/pages/projects/codeblue/index.js
+++ src/views/pages/projects/codeblue/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ScrollArea from 'react-scrollbar';
+import {Link} from 'react-router';
+import ExitIcon from 'react-icons/lib/ti/delete-outline';
+import UserIcon from 'react-icons/lib/fa/user';
+import GlobeIcon from 'react-icons/lib/fa/globe';
+import CalIcon from 'react-icons/lib/fa/calendar';
+
+import './style.scss';
+
 
 export default () => (
   <div className="page-container">
     <div className="sub-page">
       <section className="card-page">
         <ScrollArea className="scroll-container" horizontal={false} verticalScrollbarStyle={{width: '4px', marginLeft: '10px'}}>
+          <div className="page-exit">
+            <Link to="/portfolio">
+              <ExitIcon size='32'/>
+            </Link>
+          </div>
           <div className="inner-container">
-            <div className="border-block"/>
             <div className="card-title-section">
               <div className="card-title-wrap clearfix">
-                <h1 className="title">Code Blue</h1>
-                <h5 className="subtitle-section">Lets Get to Work!</h5>
+                <h1 className="title-project">Code Blue</h1>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-7 col-md-7 pad-col-project">
+
+              </div>
+              <div className="col-sm-5 col-md-5 pad-col-project">
+                <div className="block-title"><h3>Description</h3></div>
+                <ul className="project-info">
+                  <li><p><UserIcon className="icon"/>Brandon Reid</p></li>
+                  <li><p><GlobeIcon className="icon"/><a href="http://portal.cprlifeguard.com/">portal.cprlifeguard.com</a></p></li>
+                  <li><p><CalIcon className="icon"/>May 2017</p></li>
+                </ul>
+                <p className="text-justify">Aliquam euismod aliquam massa, quis eleifend dui sodales vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                <div className="tags-section">
+                  <div className="block-title"><h3>Technology</h3></div>
+                  <ul className="tags">
+                    <li><a>React</a></li>
+                    <li><a>Javascript</a></li>
+                    <li><a>CSS3</a></li>
+                    <li><a>HTML</a></li>
+                    <li><a>NoSQL</a></li>
+                    <li><a>NodeJS</a></li>
+                    <li><a>ExpressJS</a></li>
+                    <li><a>Digital Ocean</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
