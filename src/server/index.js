511af166fa161ab7ce737fed4ebbdec11b0d1b71
bar0191 <brandonreid2@my.unt.edu>
2017-07-24 19:44:08 -0500

    set up contact form and server api for smtp

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,34 @@
 const
     express     = require('express'),
     logger      = require('morgan'),
+    nodemailer  = require('nodemailer'),
     path        = require('path'),
     bodyParser  = require('body-parser'),
     serveStatic = express.static,
     http        = require('http'),
+    dotenv      = require('dotenv'),
     router      = express.Router();
 
+dotenv.load();
+
+let smtpTransport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
 
 let app = express(),
     server = http.createServer(app);
 
-
 app.set('port', process.env.PORT || 3001);
 
-// setup some middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(serveStatic( path.resolve('build') ));
 
-
-// use of express router example.
 app.use(router.get('/robots.txt', (req, res) => {
     res.sendFile('robots.txt', {
         root: path.resolve('build')
@@ -32,7 +39,24 @@ app.use(router.get('*', (req, res) => {
     res.sendFile('index.html', {
         root: path.resolve('build')
     });
-}))
+}));
+
+app.use(router.post('/send', (req, res) => {
+  let mailOptions = {
+    from: '"'+req.body.author+'" <'+req.body.email+'>',
+    to: process.env.RECIPIENT,
+    subject: 'Portfolio Message',
+    text: req.body.text,
+    html: '<b>'+req.body.text+'</b>'
+  };
+
+  smtpTransport.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+}));
 
 server.listen(app.get('port'), () => {
     console.log('Listening on: localhost:' + app.get('port'));
--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -1,6 +1,82 @@
 import React from 'react';
 import ScrollArea from 'react-scrollbar';
 
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: '',
+      text: '',
+      email: ''
+    }
+  }
+
+  handleAuthorChange = (e) => {
+    this.setState({author: e.target.value});
+  };
+
+  handleEmailChange = (e) => {
+    this.setState({email: e.target.value});
+  };
+
+  handleTextChange = (e)  => {
+    this.setState({text: e.target.value});
+  };
+
+  handleSubmit = (e)  => {
+    e.preventDefault();
+
+    fetch('/send', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        author: this.state.author,
+        text: this.state.text
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.success) {
+          this.setState({formSent: true})
+        }
+        else this.setState({formSent: false})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Your email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Send Email" />
+      </form>
+    )
+  }
+}
+
 export default () => (
   <div className="page-container">
     <div className="sub-page">
@@ -18,6 +94,10 @@ export default () => (
             <div className="col-sm-6 col-md-6 pad-col">
               <div className="block-title"><h3>Get in Touch</h3></div>
               <p>Sed eleifend sed nibh nec fringilla. Donec eu cursus sem, vitae tristique ante. Cras pretium rutrum egestas. Integer ultrices libero sed justo vehicula, eget tincidunt tortor tempus.</p>
+            </div>
+            <div className="col-sm-6 col-md-6 pad-col">
+              <div className="block-title"><h3>Contact Form</h3></div>
+              <ContactForm/>
             </div>
           </div>
         </div>
