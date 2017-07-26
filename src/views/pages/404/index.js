5b710d10c01fe7f5daddd7c0b3de22560bc30a5c
bar0191 <brandonreid2@my.unt.edu>
2017-07-26 13:38:31 -0500

    set up temp 404 page

--- src/views/pages/404/index.js
+++ src/views/pages/404/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router';
+import './style.scss';
 
 export default () => (
-  <div>
-    404 page
+  <div className="error-wrapper">
+    <div className="content">
+      <Link to="/"/>
+    </div>
   </div>
 );
