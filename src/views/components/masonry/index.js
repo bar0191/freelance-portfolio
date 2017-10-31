61b53223b28d4d5747c2d113e418aa27fae89a34
Brandon Reid <brandonreid2@my.unt.edu>
2017-10-30 23:18:58 -0500

    updated masonry for projects

--- src/views/components/masonry/index.js
+++ src/views/components/masonry/index.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router';
 import OldPort from '../../../images/oldfolio-sm.png';
 import CodeBlue from '../../../images/codeblue-sm.png';
 import Pokedex from '../../../images/pokedex-main-sm.png';
+import LevelTen from '../../../images/l10_landing.png';
+import ReactPM from '../../../images/rpm_landing.png';
 
 
 import './style.scss';
@@ -17,6 +19,28 @@ const Gallery = React.createClass({
         className={'my-gallery-class mason'}
       >
         {/*<Link to="/projects/latherbynature"><img style={imgStyle} src={LatherBy}/></Link>*/}
+        <article className="project">
+          <Link to="/projects/levelten">
+            <img src={LevelTen} role="presentation"/>
+            <span className="meta">
+            <span className="meta-inner">
+              <h1>LevelTen Site</h1>
+              <p>Redesign agency website </p>
+            </span>
+          </span>
+          </Link>
+        </article>
+        <article className="project">
+          <Link to="/projects/reactpm">
+            <img src={ReactPM} role="presentation"/>
+            <span className="meta">
+            <span className="meta-inner">
+              <h1>ReactPM</h1>
+              <p>React Password Manager </p>
+            </span>
+          </span>
+          </Link>
+        </article>
         <article className="project">
         <Link to="/projects/old-portfolio">
           <img src={OldPort} role="presentation"/>
