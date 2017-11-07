02118343f9c723bc91112b0b92c0588ac2835b0e
Brandon Reid <brandonreid2@my.unt.edu>
2017-11-07 14:59:54 -0600

    added intelwp to masonry

--- src/views/components/masonry/index.js
+++ src/views/components/masonry/index.js
@@ -7,6 +7,7 @@ import CodeBlue from '../../../images/codeblue-sm.png';
 import Pokedex from '../../../images/pokedex-main-sm.png';
 import LevelTen from '../../../images/l10_landing.png';
 import ReactPM from '../../../images/rpm_landing.png';
+import Intel from '../../../images/intel-home.png';
 
 
 import './style.scss';
@@ -20,12 +21,12 @@ const Gallery = React.createClass({
       >
         {/*<Link to="/projects/latherbynature"><img style={imgStyle} src={LatherBy}/></Link>*/}
         <article className="project">
-          <Link to="/projects/levelten">
-            <img src={LevelTen} role="presentation"/>
+          <Link to="/projects/intelwp">
+            <img src={Intel} role="presentation"/>
             <span className="meta">
             <span className="meta-inner">
-              <h1>LevelTen Site</h1>
-              <p>Redesign agency website </p>
+              <h1>IntelligenceWP</h1>
+              <p>Redesign/migration </p>
             </span>
           </span>
           </Link>
@@ -37,6 +38,17 @@ const Gallery = React.createClass({
             <span className="meta-inner">
               <h1>ReactPM</h1>
               <p>React Password Manager </p>
+            </span>
+          </span>
+          </Link>
+        </article>
+        <article className="project">
+          <Link to="/projects/levelten">
+            <img src={LevelTen} role="presentation"/>
+            <span className="meta">
+            <span className="meta-inner">
+              <h1>LevelTen Site</h1>
+              <p>Redesign agency website </p>
             </span>
           </span>
           </Link>
